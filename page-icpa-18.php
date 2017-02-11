47c210997721e70b1d684748d3e20b5b18bfaddb
Petr Stepanov <kisstherail@gmail.com>
2017-02-10 20:03:15 -0500

    Finished the Landing

--- page-icpa-18.php
+++ page-icpa-18.php
@@ -71,112 +71,112 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<h2 class="mt-4 mt-md-5">International Advisory Committee</h2>
 
 			<ul class="row list-unstyled">
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/peter-mascher.jpg" alt="Peter Mascher">
 					<div class="media-body">
 						<h5>Peter Mascher</h5>
 						McMaster University<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Ontario, Canada
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/reinhard-krause-rehberg.jpg" alt="Reinhard Krause-Rehberg">
 					<div class="media-body">
 						<h5>Reinhard Krause-Rehberg</h5>
 						Martin Luther University Halle-Wittenberg<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Leipzig, Germany
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/m-ashram-alam.jpg" alt="M. Ashram Alam">
 					<div class="media-body">
 						<h5>M. Ashram Alam</h5>
 						University of Bristol<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Bristol, UK
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/bernardo-barbellini.jpg" alt="Bernardo Barbellini">
 					<div class="media-body">
 						<h5>Bernardo Barbellini</h5>
 						Northeastern University<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Boston, USA
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/marie-france-barthe.jpg" alt="Marie France Barthe">
 					<div class="media-body">
 						<h5>Marie France Barthe</h5>
 						Centre National de la Recherche Scientifique<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Orléans, France
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/roberto-bursa.jpg" alt="Roberto S. Bursa">
 					<div class="media-body">
 						<h5>Roberto S. Bursa</h5>
 						Università degli Studi di Trento<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Trento, Italy
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/chen-zhi-quan.jpg" alt="Chen Zhi-quan">
 					<div class="media-body">
 						<h5>Chen Zhi-quan</h5>
 						Wuhan University<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Wuhan, China
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/jakub-cizek.jpg" alt="Jakub Cizek">
 					<div class="media-body">
 						<h5>Jakub Cizek</h5>
 						Charles University<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Prague, Czech Republic
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/bichitra-ganguly.jpg" alt="Bichitra Ganguly">
 					<div class="media-body">
 						<h5>Bichitra Ganguly</h5>
 						Saha Institute of Nuclear Physics<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Kolkata, India
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/christoph-pascal-hugenschmidt.jpg" alt="Christoph Pascal Hugenschmidt">
 					<div class="media-body">
 						<h5>Christoph Pascal Hugenschmidt</h5>
 						Technische Universität München<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Munich, Germany
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/gaetana-laricchia.jpg" alt="Gaetana Laricchia">
 					<div class="media-body">
 						<h5>Gaetana Laricchia</h5>
 						University College London<span class="hidden-md-up">,</span><br class="hidden-sm-down"> London, UK
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/allen-mills.jpg" alt="Allen Mills">
 					<div class="media-body">
 						<h5>Allen Mills</h5>
 						University of California<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Riverside, USA
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/yasuyuki-nagashima.jpg" alt="Yasuyuki Nagashima">
 					<div class="media-body">
 						<h5>Yasuyuki Nagashima</h5>
 						Tokyo University of Science<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Tokyo, Japan
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/henk-schut.jpg" alt="Henk Schut">
 					<div class="media-body">
 						<h5>Henk Schut</h5>
 						Delft University of Technology<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Delft, Netherlands
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/shirai-yasuharu.jpg" alt="Shirai Yasuharu">
 					<div class="media-body">
 						<h5>Shirai Yasuharu</h5>
 						Kyoto University<span class="hidden-md-up">,</span><br class="hidden-sm-down"> Kyoto, Japan
 					</div>
 				</li>
-				<li class="media mt-3 mt-md-4 media-committee">
+				<li class="media mt-3 mt-md-4 col-md-2 col-lg-4 media-committee">
 					<img class="d-flex mr-3" src="<?php echo get_template_directory_uri(); ?>/img/committee/james-sullivan.jpg" alt="James Sullivan">
 					<div class="media-body">
 						<h5>James Sullivan</h5>
@@ -295,51 +295,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 		</div>
 	</div>
 </div>
-<div class="container-wrapper container-wrapper-primary-darkest">
+<div class="container-wrapper container-wrapper-primary-darkest footer">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-
+		<div class="row justify-content-between align-items-center">
+			<div class="col-9 col-lg-10">
+				<p class="mb-0"><small><strong>Photos will be taken at the event. Please let us know if you prefer not to have your photo taken.</strong></small></p>
 			</div>
-			<div class="col-md-5">
+			<div class="col-2 col-lg-1">
+				<img class="bgsu-logo" src="<?php echo get_template_directory_uri(); ?>/img/bgsu-logo.svg" />
 			</div>
 		</div>
 	</div>
 </div>
-
-<div class="wrapper" id="full-width-page-wrapper">
-
-	<div class="<?php echo esc_html( $container ); ?>" id="content">
-
-		<div class="row">
-
-			<div class="col-md-12 content-area" id="primary">
-
-				<main class="site-main" id="main" role="main">
-
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-
-							comments_template();
-
-						endif;
-						?>
-
-					<?php endwhile; // end of the loop. ?>
-
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row end -->
-
-	</div><!-- Container end -->
-
-</div><!-- Wrapper end -->
-
-<?php get_footer(); ?>
